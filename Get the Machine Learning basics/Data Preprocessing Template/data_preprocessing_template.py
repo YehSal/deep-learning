08f34a49bed611b19b251b291898cf290c65c690
Yehia Saleh <yehia.saleh@yale.edu>
2017-11-20 01:26:24 -0500

    Missing data & categorical data

--- Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
+++ Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
@@ -7,11 +7,22 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-Y = dataset.iloc[:, -1].values
+X = dataset.iloc[:, :-1].values # Indepedent variables
+y = dataset.iloc[:, -1].values  # Dependent variable
 
-# Taking care of missing data
+# Taking care of missing data by taking the mean of the columns
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+
+# Encoding categorical data and expanding to several columns to prevent
+# machine learning equation from thinking one country is "bigger" than another
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_Y = LabelEncoder() # DV is already recognized as categorical
+y = labelencoder_Y.fit_transform(y)
