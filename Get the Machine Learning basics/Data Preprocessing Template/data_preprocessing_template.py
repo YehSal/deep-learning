4f33a0528141cc0100801839540937d9dff4bf38
Yehia Saleh <yehia.saleh@yale.edu>
2017-11-20 19:54:48 -0500

    Fit classifier to training set

--- Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
+++ Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
@@ -11,22 +11,28 @@ X = dataset.iloc[:, :-1].values # Indepedent variables
 y = dataset.iloc[:, -1].values  # Dependent variable
 
 # Taking care of missing data by taking the mean of the columns
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+# from sklearn.preprocessing import Imputer
+# imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+# imputer.fit(X[:, 1:3])
+# X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 
 # Encoding categorical data and expanding to several columns to prevent
 # machine learning equation from thinking one country is "bigger" than another
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
-labelencoder_Y = LabelEncoder() # DV is already recognized as categorical
-y = labelencoder_Y.fit_transform(y)
+# from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# labelencoder_X = LabelEncoder()
+# X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+# onehotencoder = OneHotEncoder(categorical_features = [0])
+# X = onehotencoder.fit_transform(X).toarray()
+# labelencoder_Y = LabelEncoder() # DV is already recognized as categorical
+# y = labelencoder_Y.fit_transform(y)
 
 # Split data in training set and test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature scaling to ensure no variable is dominated by the other
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test) # Since it's already fitted
